427989a5c355255ed6695d7230f4a8b514163f36
Mendel Ba <33570785+MENDEL-BA@users.noreply.github.com>
2022-05-09 15:49:49 +0000

    correction of data

--- index.js
+++ index.js
@@ -17,17 +17,36 @@ var jsn = [{
 
 var header="Name "+"\t"+" School "+"\t"+"Marks "+"\n";
 
-writeStream.write(header);
-//writeStream.write(jsn);
-
-//writeStream.close();
-var data=''; 
-for (var i = 0; i < jsn.length; i++) {
+var data= '' + header; 
+for (let i = 0; i < jsn.length ; i++) {
     data=data+jsn[i].name+'\t'+jsn[i].school+'\t'+jsn[i].marks+'\n';
  }
+
 fs.appendFile('file.xls', data, (err) => {
     if (err) throw err;
     console.log('File created');
  });
 
 writeStream.close();
+
+
+ /* const fileName = `${uuidv4()}.xlsx`
+        var writeStream = fs.createWriteStream(targetFolder+fileName)
+        writeStream.write(header);
+
+        if (!fs.existsSync(targetFolder)) {
+            fs.mkdirSync(targetFolder, { recursive: true })
+        }
+        console.log("mon fileName good "+fileName)
+
+        var data=''; 
+        for (var i = 0; i < jsn.length; i++) {
+            data=data+containerInfos[i].container_number+'\t'+containerInfos[i].container_product_is_chemical+'\t'+containerInfos[i].container_product_long_label+'\n';
+        }
+        fs.appendFile(fileName, data, (err) => {
+            if (err) throw err;
+            console.log('File created');
+        });
+
+        writeStream.close();
+         */
